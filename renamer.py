42ca2896021373d1c339f3d7dbd945fb9511874c
Andrew Davidson <davidsonac@Andrews-Mac-mini.local>
2022-04-04 03:49:05 -0700

    fix: rename jpgs too

--- renamer.py
+++ renamer.py
@@ -14,9 +14,14 @@ def recursive_add_mapping(curr_path, curr_name):
                 global photo_idx
                 global orig_names_to_mod_names
                 filename = f.split(".")[0]
+                fileext = f.split(".")[1]
                 new_name = curr_name + "_" + str(photo_idx)
                 photo_idx += 1
                 orig_names_to_mod_names[filename]= new_name
+                if args.force:
+                    os.rename(curr_path + "/" + f, curr_path + "/" + new_name + "." + fileext)
+                    pass
+                print("\033[1;37;40mrenamed " + curr_path + "/" + f + "  to  " + curr_path + "/" + new_name + "." + fileext)
         else:
             recursive_add_mapping(new_path, curr_name+"_"+f)
 
@@ -26,27 +31,42 @@ def clean_up_raw_folder(raw_path):
         fileext = f.split(".")[1]
         mapping = orig_names_to_mod_names.get(filename)
         if mapping and mapping != "":
-            #os.rename(raw_path + "/" + f, raw_path + "/" + mapping + "." + fileext)
+            if args.force:
+                os.rename(raw_path + "/" + f, raw_path + "/" + mapping + "." + fileext)
+                pass
             global renamed_count
             renamed_count += 1
             print("\033[1;37;40mrenamed " + raw_path + "/" + f + "  to  " + raw_path + "/" + mapping + "." + fileext)
         else:
-            #os.remove(raw_path + "/" + f)
+            if args.force:
+                os.remove(raw_path + "/" + f)
+                pass
             global delete_count
             delete_count += 1
             print("\033[1;31;40mdeleted " + raw_path + "/" + f)
 
-parser = argparse.ArgumentParser(description='Given the absolute path of a base directory containing images sorted into "raw" and "jpg" folders, renames the raw photos after the structure of subdirectories in jpg, deleting RAW files whose jpgs were deleted.')
+parser = argparse.ArgumentParser(description='Given the absolute path of a base directory containing images sorted into "raw" and "unprocessed" folders, renames the raw photos after the structure of subdirectories in unprocessed, deleting RAW files whose jpgs were deleted. Also renames the jpgs.')
 parser.add_argument('directory', metavar='Dir', type=str, nargs=1,
-                    help='absolute path of base photo directory.  Must contain raw and jpg subdirectories')
+                    help='absolute path of base photo directory.  Must contain raw and unprocessed subdirectories')
+parser.add_argument("-f", "--force", help="enables changes to be written.  Defaults to false for preview mode. Cannot be undone",
+                    action="store_true")
 
 args = parser.parse_args()
+
+if args.force:
+    print("Force flag detected. Changes will be made.")
+else:
+    print("The force flag was not set using '-f', so no changes will be made.  Preview mode.")
 
 base_dir = args.directory[0]
 dir_names = base_dir.split("/")
 base_dir_name = dir_names[-1]
 if base_dir_name == '':
     base_dir_name = dir_names[-2]
-recursive_add_mapping(base_dir+"/jpg", base_dir_name)
+recursive_add_mapping(base_dir+"/unprocessed", base_dir_name)
 clean_up_raw_folder(base_dir+"/raw")
 print("\033[1;36;40m" + str(renamed_count) + " files renamed and " + str(delete_count) + " deleted.")
+if args.force:
+    print("Force flag detected. Changes were made.")
+else:
+    print("The force flag was not set using '-f', so no changes were made.  Preview mode.")
