5293f5bf44f4c3c9e0eccd09cc4af10f84d858a1
Andrew Davidson <davidsonac@Andrews-Mac-mini.local>
2022-04-07 11:26:56 -0700

    fix: add separate counts to print

--- renamer.py
+++ renamer.py
@@ -4,7 +4,8 @@ import os
 photo_idx = 1
 orig_names_to_mod_names = {}
 delete_count = 0
-renamed_count = 0
+renamed_jpg_count = 0
+renamed_raw_count = 0
 
 def recursive_add_mapping(curr_path, curr_name):
     for f in os.listdir(curr_path):
@@ -19,13 +20,15 @@ def recursive_add_mapping(curr_path, curr_name):
                 photo_idx += 1
                 orig_names_to_mod_names[filename]= new_name
                 if args.force:
+                    global renamed_jpg_count
+                    renamed_jpg_count += 1
                     os.rename(curr_path + "/" + f, curr_path + "/" + new_name + "." + fileext)
-                    pass
                 print("\033[1;37;40mrenamed " + curr_path + "/" + f + "  to  " + curr_path + "/" + new_name + "." + fileext)
         else:
             recursive_add_mapping(new_path, curr_name+"_"+f)
 
 def clean_up_raw_folder(raw_path):
+    global renamed_raw_count
     for f in os.listdir(raw_path):
         filename = f.split(".")[0]
         fileext = f.split(".")[1]
@@ -33,14 +36,11 @@ def clean_up_raw_folder(raw_path):
         if mapping and mapping != "":
             if args.force:
                 os.rename(raw_path + "/" + f, raw_path + "/" + mapping + "." + fileext)
-                pass
-            global renamed_count
-            renamed_count += 1
+            renamed_raw_count += 1
             print("\033[1;37;40mrenamed " + raw_path + "/" + f + "  to  " + raw_path + "/" + mapping + "." + fileext)
         else:
             if args.force:
                 os.remove(raw_path + "/" + f)
-                pass
             global delete_count
             delete_count += 1
             print("\033[1;31;40mdeleted " + raw_path + "/" + f)
@@ -65,7 +65,7 @@ if base_dir_name == '':
     base_dir_name = dir_names[-2]
 recursive_add_mapping(base_dir+"/unprocessed", base_dir_name)
 clean_up_raw_folder(base_dir+"/raw")
-print("\033[1;36;40m" + str(renamed_count) + " files renamed and " + str(delete_count) + " deleted.")
+print("\033[1;36;40m" + str(renamed_jpg_count) + " jpg files renamed, " + str(renamed_raw_count) + " raw files renamed, and " + str(delete_count) + " deleted.")
 if args.force:
     print("Force flag detected. Changes were made.")
 else:
